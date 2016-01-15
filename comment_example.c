347143cfd73bd4f88021f27ff10811c365e047a2
Deepak Sharma <depthgr8@gmail.com>
2016-01-16 01:11:20 +0530

    First example - Documentation

---
+++ comment_example.c
@@ -0,0 +1,39 @@
+//	This is an example of single line comment
+
+/*	This is an
+	example of multiple 
+	line comment,
+	Here you dont need to
+	comment each and every
+	line.
+
+	Only first and last lines
+	need to have a valid comment
+	opening and closing.
+*/
+
+//	You can also comment like this	- This looks good	//
+/* 	You can also end your comment like this		*/
+
+// 	If you want to separate parts of a program you can use underscore or hyphen, as given below  	//
+
+//------------------------------ Real comments starts now -----------------------------//
+
+/* 	Lets see the examples of comment section
+	Date created:		January 16, 2015
+	Date modified:		February 1, 2016
+	Author:			Deepak Sharma
+	Purpose:		To prepare documentation to enhance readability
+*/
+
+//------------------------------ Reak program starts now ------------------------------//
+
+#include <stdio.h>
+int main()
+{
+	printf("\t This is a paragon of commenting \n");
+	return 0;
+}
+
+//-------------------------------- Program ends here ----------------------------------//
+
