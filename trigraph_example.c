6e7bf838e0e86ac08b3a91547525f16101883979
Deepak Sharma <depthgr8@gmail.com>
2016-01-17 04:39:30 +0530

    2 new directories

---
+++ trigraph_example.c
@@ -0,0 +1,34 @@
+/*
+	This program provides
+	the three example implementation
+	of trigraph characters.
+
+*/
+
+/*	Trigraph characters used
+	1).	[
+	2).	]
+	3).	\
+*/
+
+//-------------------- Real program starts from here -----------//
+
+#include <stdio.h>
+int main()
+{
+	// This will print [
+	printf("??(");
+
+	// This will print ]
+	printf("??)");
+
+	// This will not print \ (Backslash)
+	//	printf("??/");
+
+	// To print \ (Backslash) you need to use escape characters
+	printf("\\");
+
+	return 0;
+}
+
+//------------------- Programs ends here -----------------------//
