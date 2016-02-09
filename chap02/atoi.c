129480caeb9caf3f6a49d357f16ece594919c9c2
Deepak Sharma <depthgr8@gmail.com>
2016-02-09 13:51:42 +0530

    Preparing documentation

--- chap02/atoi.c
+++ chap02/atoi.c
@@ -1,14 +1,19 @@
+/*	This source code provides a demo program
+	to convert string into its integer value	*/
 # include <stdio.h>
+
+//	Function prototype
 int atoi(char s[]);
 main ()
 {
-	char str[6] = "654123";
+	char str[6] = "a";
 	printf("Integer value of given string is %d \n",atoi(str));
 }
+//	Function definition
 int atoi(char st[])
 {
 	int i,n;
-
+//	Program conversion logic
 	n = 0;
 	for(i=0;st[i] >= '0' && st[i] <= '9'; ++i)
 		n = 10 * n + (st[i] - '0');
