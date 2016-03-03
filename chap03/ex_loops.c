6ca169ab0a40d20abfe50ba8f5e7a398858a5fd9
Deepak Sharma <depthgr8@gmail.com>
2016-03-03 08:30:56 +0530

    Added new example

--- chap03/ex_loops.c
+++ chap03/ex_loops.c
@@ -4,8 +4,18 @@
 
 main()
 {
-	int i,j,k,lim = 10;
+	int i = 1,j = 0,k,lim = 10;
 
-	while(i<10)
+	while(i <= 10)
 	{
-		printf(""
+		printf("While iteration no. %d \n",i);
+		i++;
+	}
+	do
+	{
+		printf("This iteration will always be executed \n");
+	}
+	while( j!= 0);
+	for(k = 0; k < lim ; k++)
+		printf("For iteration no. %d \n",k);
+}
